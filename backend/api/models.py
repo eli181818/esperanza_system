161a35e4c9f33f3d7697985f484078073410a1ef
Janna Elijah <jannaelijahapenuela@iskolarngbayan.pup.edu.ph>
2025-10-06 20:51:39 +0800

    Added first, middle, and last names on DB; Fixed calculated age function

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,12 +4,15 @@ from django.utils import timezone
 from datetime import date
 
 class HCStaff(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
     staff_pin = models.CharField(max_length=4, unique=True)
 
 class Patient(models.Model):    
     patient_id = models.CharField(max_length=20, unique=True)
-    name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField(null=True, blank=True)
     gender = models.CharField(max_length=6, choices=[('Male', 'Male'), ('Female', 'Female')])
     contact = models.CharField(max_length=11, default='N/A')
     address = models.TextField(max_length=450)
@@ -18,7 +21,16 @@ class Patient(models.Model):
     pin = models.CharField(max_length=4)
     fingerprint_id = models.CharField(max_length=4, null=True, blank=True)
 
-    def save(self, *args, **kwargs): # Auto-generate the patient_id 
+    def save(self, *args, **kwargs): # Auto-generate the patient_id; Calculated age
+        if self.birthdate:
+            today = date.today()
+            calculated_age = today.year - self.birthdate.year
+            
+            # Adjust if birthday hasn't happened yet this year
+            if (today.month, today.day) < (self.birthdate.month, self.birthdate.day):
+                calculated_age -= 1
+            self.age = calculated_age
+            
         if not self.patient_id:
             today = timezone.now().date()
             yyyymmdd = today.strftime("%Y%m%d")
@@ -28,16 +40,6 @@ class Patient(models.Model):
             self.patient_id = f"P-{yyyymmdd}-{count_today:03d}"
         super().save(*args, **kwargs)
         
-    @property
-    def age(self):
-        if self.birthdate:
-            today = date.today()
-            age = today.year - self.birthdate.year
-            # Adjust if birthday hasn't happened yet this year
-            if (today.month, today.day) < (self.birthdate.month, self.birthdate.day):
-                age -= 1
-            return age
-        return None
 
 class VitalSigns(models.Model):
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='vital_signs') # Link to Patient model
