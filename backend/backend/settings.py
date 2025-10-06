497e4893cb9e9d268979af621cf2e9bae78c3178
Janna Elijah <jannaelijahapenuela@iskolarngbayan.pup.edu.ph>
2025-10-06 09:18:58 +0800

    Update settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,11 +94,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'patient_espe',
-        'USER': 'root',
-        'PASSWORD': 'Jea.penuela18',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
         'OPTIONS': {
             'charset': 'utf8mb4',
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
